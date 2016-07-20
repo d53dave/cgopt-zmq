f871798def6df07c575fd7fa5132f83bca4139f9
Dave <dave@d53dev.net>
2016-07-20 09:32:09 +0200

    Added file logger sink

--- src/CSAOptMessageQueue.cpp
+++ src/CSAOptMessageQueue.cpp
@@ -3,7 +3,7 @@
 CSAOpt::MessageQueue::MessageQueue(int port) {
     std::vector<spdlog::sink_ptr> sinks;
     sinks.push_back(std::make_shared<spdlog::sinks::stdout_sink_st>());
-//    sinks.push_back(std::make_shared<spdlog::sinks::rotating_file_sink>("csaopt_msqqueue", "log", 1024 * 1024 * 5, 10));
+    sinks.push_back(std::make_shared<spdlog::sinks::rotating_file_sink_st>("csaopt_msqqueue", "log", 1024 * 1024 * 5, 10));
     this->logger = std::make_shared<spdlog::logger>("name", begin(sinks), end(sinks));
     spdlog::register_logger(this->logger);
 
