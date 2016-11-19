af5297858c8eb650012e07101f3a4a1ac7eac0cd
Dave <dave@d53dev.net>
2016-11-19 20:40:51 +0100

    Made sure to get unsigned ints from the program arguments

--- src/main.cpp
+++ src/main.cpp
@@ -10,27 +10,33 @@ CSAOpt::MessageQueue *msgQueue;
 
 void signal_handler(int signal) {
     gSignalStatus = signal;
-//    if(msgQueue) {
-//        msgQueue->abort();
-//    }
 }
 
 int main(int argc, char* argv[]) {
-    int port = 11551;
-    int port2 = 12559;
+    unsigned int port = 11551;
+    unsigned int port2 = 12559;
     if(argc > 2) {
-        port = std::stoi(std::string{argv[1]});
-        port2 = std::stoi(std::string{argv[2]});
+        port = (unsigned int) std::stoi(std::string{argv[1]});
+        port2 = (unsigned int) std::stoi(std::string{argv[2]});
     }
+
 
     std::signal(SIGINT, signal_handler);
 
     msgQueue = new CSAOpt::MessageQueue(port, port2);
 
-    while(gSignalStatus == 0) {
+    std::vector<std::string> arguments(argv, argv + argc);
+    for(auto && arg : arguments) {
+        if(arg == "-v" || arg == "--verbose") {
+            msgQueue->logDebug();
+        }
+    }
+
+    while(gSignalStatus == 0 && !msgQueue->finished) {
+        // just sit around and wait for exit
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
-    std::cout << "Deleting msgQueue" << std::endl;
+
     delete msgQueue;
-    std::cout << "Done." << std::endl;
+    std::cout << "Bye." << std::endl;
 }
