bef61fafa20e21f8ffee2e82c84a660ceabb99b3
Dave <dave@d53dev.net>
2017-05-29 22:12:30 +0200

    Refactored variable names

--- test/PlumbingTest.cpp
+++ test/PlumbingTest.cpp
@@ -16,8 +16,7 @@ SCENARIO("Workers want to register and unregister with the Messagequeue", "") {
     int plumbingsPort = 12378;
 
     GIVEN("A messagequeue") {
-        CSAOpt::MessageQueue messageQueue(tidingsPort, plumbingsPort);
-
+        CSAOpt::MessageQueue* messageQueue = new CSAOpt::MessageQueue(tidingsPort, plumbingsPort);
 
         zmqpp::context context;
         zmqpp::socket_type type = zmqpp::socket_type::req;
@@ -40,6 +39,7 @@ SCENARIO("Workers want to register and unregister with the Messagequeue", "") {
             writePackedMessage(pipe, message);
 
             req << pipe.getData();
+
             socket.send(req);
 
             THEN("The server responds with OK"){
@@ -51,15 +51,15 @@ SCENARIO("Workers want to register and unregister with the Messagequeue", "") {
 
                 kj::std::StringPipe newPipe(recvdata);
                 ::capnp::PackedMessageReader recvMessage(newPipe);
-
                 Plumbing::Reader recvPlumbing = recvMessage.getRoot<Plumbing>();
+
 
                 REQUIRE(strcmp(plumbing.getId().cStr(), recvPlumbing.getId().cStr()) == 0);
                 REQUIRE(recvPlumbing.getType() == Plumbing::Type::ACK);
             }
         }
 
-        WHEN("A worker want's to leave after it jointed"){
+        WHEN("A worker wants to leave after it jointed"){
             zmqpp::message req;
 
             ::capnp::MallocMessageBuilder message;
@@ -109,35 +109,38 @@ SCENARIO("Workers want to register and unregister with the Messagequeue", "") {
         }
 
         WHEN("A worker want's to leave but never joined"){
-            zmqpp::message req;
+            zmqpp::message reqBad;
 
-            ::capnp::MallocMessageBuilder message;
-            Plumbing::Builder plumbing = message.initRoot<Plumbing>();
+            ::capnp::MallocMessageBuilder messageBad;
+            Plumbing::Builder plumbingBad = messageBad.initRoot<Plumbing>();
 
-            std::time_t t = std::time(0);
-            setData(plumbing, "neverJoinedId", t, "worker1", Plumbing::Type::UNREGISTER);
+            std::time_t tBad = std::time(0);
+            setData(plumbingBad, "1239", tBad, "worker7", Plumbing::Type::UNREGISTER);
 
-            kj::std::StringPipe pipe;
-            writePackedMessage(pipe, message);
+            kj::std::StringPipe pipeBad;
+            writePackedMessage(pipeBad, messageBad);
 
-            req << pipe.getData();
-            socket.send(req);
+            reqBad << pipeBad.getData();
+
+            socket.send(reqBad);
+
             THEN("The server responds with Error"){
-                zmqpp::message resp;
-                socket.receive(resp);
+                zmqpp::message respBad;
+                socket.receive(respBad);
 
-                std::string recvdata;
-                resp >> recvdata;
+                std::string recvdataBad;
+                respBad >> recvdataBad;
 
-                kj::std::StringPipe newPipe(recvdata);
-                ::capnp::PackedMessageReader recvMessage(newPipe);
+                kj::std::StringPipe newPipeBad(recvdataBad);
+                ::capnp::PackedMessageReader recvMessageBad(newPipeBad);
+                Plumbing::Reader recvPlumbingBad = recvMessageBad.getRoot<Plumbing>();
 
-                Plumbing::Reader recvPlumbing = recvMessage.getRoot<Plumbing>();
 
-                REQUIRE(strcmp(plumbing.getId().cStr(), recvPlumbing.getId().cStr()) == 0);
-                REQUIRE(recvPlumbing.getType() == Plumbing::Type::ERROR);
+                REQUIRE(strcmp(plumbingBad.getId().cStr(), recvPlumbingBad.getId().cStr()) == 0);
+                REQUIRE(recvPlumbingBad.getType() == Plumbing::Type::ERROR);
             }
         }
 
+        delete messageQueue;
     }
 }
