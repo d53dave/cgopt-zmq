88fd5ef1934afb6feb327cb28a5490b476ec394a
Dave <dave@d53dev.net>
2017-07-17 11:12:30 +0200

    Added basic, non-working unit-tests

--- tests/server/test_messagequeue_plumbing.py
+++ tests/server/test_messagequeue_plumbing.py
@@ -1,17 +1,66 @@
-from unittest import TestCase
+"""Test suite for the plumbing part of the message queue"""
 
-class TestJoin(TestCase):
+import pytest
+import os
+import arrow
+import zmq
+from server import messagequeue
+import capnp
+capnp.remove_import_hook()
+
+THIS_DIR = os.path.dirname(__file__)
+plumbing_capnp = capnp.load(
+    os.path.join(THIS_DIR, '../../lib/capnp/src/plumbing.capnp'))
+
+PLUMBING_PORT = 9988
+
+
+class QueueClient:
+    def __init__(self):
+        self.ctx = zmq.Context()
+        self.socket = self.ctx.socket(zmq.REQ)
+
+    def send_request(self, message):
+        self.socket.connect('tcp://127.0.0.1:' + str(PLUMBING_PORT))
+        self.socket.send(message)
+        return self.socket.recv()
+
+
+@pytest.fixture
+def csaopt_server():
+    '''Returns an instance of the messagequeue server'''
+    return messagequeue.RepReqServer(PLUMBING_PORT, PLUMBING_PORT + 1)
+
+
+@pytest.fixture
+def my_client():
+    '''Returns a client instance'''
+    return QueueClient()
+
+
+def test_join(csaopt_server, my_client):
     """A worker wants to join the hive"""
-    pass
+    message = plumbing_capnp.Plumbing.new_message()
+    message.id = '1234'
+    message.sender = 'worker1'
+    message.timestamp = arrow.utcnow().timestamp
+    message.type = 'register'
 
-class TestLeaveAfterJoin(TestCase):
-    """A worker wants to leave after it joined"""
-    pass
+    csaopt_server.start_loop()
+    response = my_client.send_request(message.to_bytes())
 
-class TestDoubleJoin(TestCase):
-    """A worker tries to join after it already joined"""
-    pass
+    assert response.type == 'ack'
 
-class TestLeaveWithoutJoin(TestCase):
-    """A worker tries to leave before it joined"""
-    pass
+# def TestLeaveAfterJoin():
+#     """A worker wants to leave after it joined"""
+#     pass
+
+
+# def TestDoubleJoin():
+#     """A worker tries to join after it already joined"""
+#     pass
+
+
+# def TestLeaveWithoutJoin():
+#     """A worker tries to leave before it joined"""
+#     pass
